a34ebcbc3c44fc4af5e0f9f5712a7a18eee613ef
egwuenu gift <laurandidi21@gmail.com>
2017-09-21 23:19:02 +0100

    added new projects :fire:

---
+++ ColorToggle/toggle.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("button");
+// var isPurple = false;
+
+
+// button.addEventListener("click", function(){
+// 	if(isPurple){
+// 		document.body.style.background = "white";
+// 	} else {
+// 		document.body.style.background = "purple";
+// 	}
+// 	isPurple = !isPurple;
+// });
+
+
+button.addEventListener("click", function(){
+	document.body.classList.toggle("purple");
+});
---
+++ array set/array.js
@@ -0,0 +1,10 @@
+function printReverse(arr) {
+    for(var i = arr.length - 1; i >= 0; i--){
+        console.log(arr[i]);
+    }
+}
+printReverse([3,6,2,5]);
+
+
+// is UNiforn function starts here
+function isUniform
