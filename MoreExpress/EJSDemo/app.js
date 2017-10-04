aab77464e3fd64748dd1548b3c926625ef48bf2d
egwuenu gift <laurandidi21@gmail.com>
2017-10-04 16:54:05 +0100

    completed expressdemo  :zap: :fire:

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,6 +2,17 @@ var express = require("express");
 
 var app = express()
 
+app.get('/', function(req, res){
+	res.render("home.ejs");
+
+});
+
+
+app.get('/fallinlovewith/:thing', function(req, res){
+	var thing = req.params.thing;
+	res.render("love.ejs", {thingVar : thing});
+
+});
 
 
 
