aa7d28d576c7feb490a69925d11df6c86f41ce64
egwuenu gift <laurandidi21@gmail.com>
2017-10-05 14:21:30 +0100

    added partials :zap:

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,16 +2,31 @@ var express = require("express");
 
 var app = express()
 
+// tells express that there's apublic folder for all assets
+app.use(express.static("public"));
+
+// tells express that the default template enginge is ejs
+app.set("view engine", "ejs");
+
 app.get('/', function(req, res){
-	res.render("home.ejs");
+	res.render("home");
 
 });
 
-
 app.get('/fallinlovewith/:thing', function(req, res){
 	var thing = req.params.thing;
-	res.render("love.ejs", {thingVar : thing});
+	res.render("love", {thingVar : thing});
 
+});
+
+app.get('/posts', function(req, res){
+	var post = [
+		{title: "Bants", author: "Susy"},
+		{title: "Akah Nnani", author: "And Bae"},
+		{title: "Sissy Yemmie", author: "Vlogs"},
+	];
+
+	res.render("posts.ejs", {post: posts});
 });
 
 
